a39b10f4c0d5f8b370ee8537bb264eb346b59d12
vu <anhuvdinh@gmail.com>
2021-11-11 20:15:20 -0600

    removed skip_before_action from CompaniesController

--- app/controllers/api/companies_controller.rb
+++ app/controllers/api/companies_controller.rb
@@ -1,5 +1,4 @@
 class Api::CompaniesController < ApplicationController
-    skip_before_action :confirm_authentication, only: [:get_children]
 
     def index
         user_companies = current_user.companies.filter{|company| company.is_parent == true}
