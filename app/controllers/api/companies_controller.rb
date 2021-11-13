5e416ad91eb07a3dcfb76849ed80d2261d0122b7
vu <anhuvdinh@gmail.com>
2021-11-13 02:13:52 -0600

    added untested code for creating new company along with associated employee and user tables

--- app/controllers/api/companies_controller.rb
+++ app/controllers/api/companies_controller.rb
@@ -16,6 +16,21 @@ class Api::CompaniesController < ApplicationController
     end
 
     def create
+        parent_company = Company.find_by(name: params[:parent_name])
+        new_company = Company.create(company_params, parent_id: parent_company.id)
+        if new_company
+            user_employee_id = current_user.employees.first.id
+            CompanyEmployee.create(company_id: new_company.id, employee_id: user_employee_id)
+            EmployeeUser.create(employee_id: user_employee_id, user_id: current_user.id)
+            render json: new_company, status: :created
+        else
+            render json: {error: "Failed to create company: #{params: name}"}, status: :unprocessable_entity 
+        end
+    end
 
+    private
+
+    def company_params
+        params.permit(:name, :street, :city, :state, :zipcode, :established, :is_parent, :company_structure, :company_type, :company_model, :logo, :description)
     end
 end
