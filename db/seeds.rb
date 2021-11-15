f16089eabe816967fbfa13a5b84bbd710b52f7ae
vu <anhuvdinh@gmail.com>
2021-11-15 17:11:37 -0600

    adjusted seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,15 +14,34 @@ industries = ["Agriculture, forestry, hunting, and fishing", "Mining", "Utilitie
 # comp1 = Company.create(name: "coa", location: "0001 seed, Pearland, TX, #{rand(10000..99999)}", description: "1st seed", is_parent: [true, false].sample, is_active: [true, false].sample, parent_id: rand(0..5), established: Date.today.advance(days: rand(-1000..1000)), structure: structures.sample, organization: organizations.sample, industry: industries.sample)
 # applicant1 = Applicant.create(first_name: "vu", middle_name: "t", last_name: "dinh", extra_info: "nothing", application_company: comp1.id)
 # Employee.create(first_name: "vu", middle_name: "t", last_name: "dinh")
-# employee1 = Employee.find_by(first_name: "vu")
+employee1 = Employee.find_by(first_name: "vu")
 # user1 = User.create(username: "vu", password: "vu", is_disabled: false, step_verify: false, verify_type: "email")
 # CompanyEmployee.create(company_id: comp1.id, employee_id: employee1.id)
 # EmployeeUser.create(user_id: user1.id, employee_id: employee1.id)
 
 10.times do
-employee1 = Employee.find_by(first_name: "vu")
-comp = Company.create(name: "co#{rand(1..25)}", location: "#{rand(1000..9999)} SomeStreet, Pearland, TX, #{rand(10000..99999)}", description: "test company description", is_parent: [true, false].sample, is_active: [true, false].sample, parent_id: rand(0..5), established: Date.today.advance(days: rand(-1000..1000)), structure: structures.sample, organization: organizations.sample, industry: industries.sample)
-CompanyEmployee.create(company_id: comp.id, employee_id: employee1.id)
+puts "new company"
+comp = Company.new(name: "co#{rand(1..25)}", location: "#{rand(1000..9999)} SomeStreet, Pearland, TX, #{rand(10000..99999)}", description: "test company description", parent_id: assign_parent_id, is_parent: assign_is_parent, is_active: [true, false].sample, established: Date.today.advance(days: rand(-1000..1000)), structure: structures.sample, organization: organizations.sample, industry: industries.sample)
+    if comp.valid?
+        comp.save
+        CompanyEmployee.create(company_id: comp.id, employee_id: employee1.id)
+        puts "created"
+    end
+end
+
+def assign_parent_id
+    possible_parent = rand(0..5)
+    if possible_parent == 0
+        assign_is_parent(true)
+        0
+    else
+        assign_is_parent(false)
+        possible_parent
+    end
+end
+
+def assign_is_parent(boolean)
+    boolean
 end
 
 puts "seeding done"
